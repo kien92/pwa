a67f0c1fc86832a12eeeb9b397674fb3650e6fb8
Mai Trung Kiên (VH-KSKienmt1M-MB) <kienmt1@vingroup.net>
2020-10-06 23:26:12 +0700

    Updates

---
+++ precache-manifest.8e7bbacb5dae68b01f93fd76e3dc36e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec00e13097525326595060ab277c8a38",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "462288ed0453f02cad3e",
+    "url": "/pwa/static/css/main.7327811c.chunk.css"
+  },
+  {
+    "revision": "c2205647423661a3dd75",
+    "url": "/pwa/static/js/2.1ac1f578.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pwa/static/js/2.1ac1f578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462288ed0453f02cad3e",
+    "url": "/pwa/static/js/main.29057ed9.chunk.js"
+  },
+  {
+    "revision": "cffe3669d5dfd3c45b99",
+    "url": "/pwa/static/js/runtime-main.9baa76eb.js"
+  }
+]);
